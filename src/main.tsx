5c31a4954d2040aac2bf7363b3a666e058382f2d
Mark c <mjcduke@gmail.com>
2025-05-14 07:11:49 -0400

    Add aggressive white background fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './custom-styles.css'
+import './custom-backgrounds.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
